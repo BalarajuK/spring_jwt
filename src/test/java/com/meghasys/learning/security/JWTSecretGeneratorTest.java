9b9cb5cdb80321070203f42c6584d86065ff922a
Balaraju Kadukuntla <balaraju004@gmail.com>
2025-04-19 21:36:07 -0400

    Spring security with JWT

--- src/test/java/com/meghasys/learning/security/JWTSecretGeneratorTest.java
+++ src/test/java/com/meghasys/learning/security/JWTSecretGeneratorTest.java
@@ -5,6 +5,9 @@ import jakarta.xml.bind.DatatypeConverter;
 import org.junit.jupiter.api.Test;
 
 import javax.crypto.SecretKey;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 
 public class JWTSecretGeneratorTest {
 
@@ -13,5 +16,14 @@ public class JWTSecretGeneratorTest {
         SecretKey key = Jwts.SIG.HS512.key().build();
         String encodedKey = DatatypeConverter.printHexBinary(key.getEncoded());
         System.out.println(encodedKey);
+    }
+
+    @Test
+    public void generateRSASecretKey(){
+        KeyPair keyPair = Jwts.SIG.RS512.keyPair().build();
+        PrivateKey privateKey = keyPair.getPrivate();
+        PublicKey publicKey = keyPair.getPublic();
+        System.out.println("Private Key:"+DatatypeConverter.printHexBinary(privateKey.getEncoded()));
+        System.out.println("Public Key:"+DatatypeConverter.printHexBinary(publicKey.getEncoded()));
     }
 }
